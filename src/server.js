063e410f722889ad59f145fb4f21d8823d934432
Felipe Soto Correa <pipesoto13@gmail.com>
2021-03-13 21:53:35 -0500

    Create supplier entity

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const { connect } = require('./db');
 const userRouter = require('./routes/user');
+const supplierRouter = require('./routes/supplier');
 const { auth } = require('./utils/auth');
 
 const port = 8000;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/users', userRouter);
+app.use('/suppliers', supplierRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
