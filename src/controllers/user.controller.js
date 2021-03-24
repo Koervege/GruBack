e2a7893cf21974e73e22c372bdbbe04b6c0796bd
Carlos <carlos_arce93@hotmail.com>
2021-03-24 16:09:29 -0500

    Fix double variable declaration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,7 +11,7 @@ module.exports = {
       let userType = '';
       let validUser = await Client.findOne({ email });
 
-      let userType = 'client'
+      userType = 'client'
 
       if(!validUser) {
         validUser = await Supplier.findOne({ email })
